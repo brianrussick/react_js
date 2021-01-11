540f1484c9c2c0c0bdbd60f8e7eb8cfa28a00b2f
brianrussick <brianrussick@gmail.com>
2021-01-11 15:25:31 -0500

    first commit

---
+++ my_first_app/src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
+const App = () => {
+    return (
+<div className="ui container comments">
+    <ApprovalCard>
+        <CommentDetail 
+            author="Sam" 
+            timeAgo="Today at 4:45PM" 
+            content="Nice blog post"
+            avatar={faker.image.image()}
+        />
+    </ApprovalCard>
+    <ApprovalCard>
+        <CommentDetail 
+            author="Alex" 
+            timeAgo="Today at 2:00AM" 
+            content="I like the subject"
+            avatar={faker.image.image()}
+        />
+    </ApprovalCard>
+    <ApprovalCard>
+        <CommentDetail 
+            author="Jane" 
+            timeAgo="Yesterday at 5:00PM" 
+            content="I like the writing"
+            avatar={faker.image.image()}
+        />
+    </ApprovalCard>
+</div>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'))
