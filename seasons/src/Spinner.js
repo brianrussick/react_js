9689493bb08a7a955c7cddf8726cf685b092b395
brianrussick <brianrussick@gmail.com>
2021-01-18 12:57:31 -0500

    Geolocation_API_react_js_app

---
+++ seasons/src/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Spinner = (props) => {
+    return (
+<div className="ui active dimmer">
+    <div className="ui big text loader">{props.message}</div>
+</div>
+
+    );
+}
+
+export default Spinner;
