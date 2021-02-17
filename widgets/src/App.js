019d97e88e2f88d49ac2415fcff9931059c1a46c
brianrussick <brianrussick@gmail.com>
2021-02-17 11:59:27 -0500

    Wikipedia_Search_API_react_js_app
    git commit -m Wikipedia_Search_API_react_js_app

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
 const items = [
     {
@@ -17,10 +21,47 @@ const items = [
     },
 ];
 
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'A Shade of Blue',
+        value: 'blue'
+    }
+];
+
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
+    const [selected, setSelected] = useState(options[0]);
+
     return (
     <div>
-        <Search />
+        <Header />
+        <Route path="/">
+          <Accordion items={items} />
+        </Route>
+        <Route path="/list">
+          <Search />
+        </Route>
+        <Route path="/dropdown">
+          <Dropdown 
+            label="Select a color"
+            options={options}
+            selected={selected}
+            onSelectedChange={setSelected}
+          />
+        </Route>
+        <Route path="/translate">
+          <Translate />
+        </Route>
     </div>
     );
 };
