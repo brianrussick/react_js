8947ac20d76ae0260e229cb9ef5728af44e6cba4
brianrussick <brianrussick@gmail.com>
2021-03-09 12:02:05 -0500

    React_Redux_Thunk - JSONPlaceholder REST API

--- react_redux_thunk/src/actions/index.js
+++ react_redux_thunk/src/actions/index.js
@@ -1,4 +1,10 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async dispatch => {
+  await dispatch(fetchPosts());
+  
+};
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
@@ -6,8 +12,19 @@ export const fetchPosts = () => async dispatch => {
     dispatch({ type: 'FETCH_POSTS', payload: response.data })
   };
 
-  export const fetchUser = (id) => async dispatch => {
-      const response = await jsonPlaceholder.get(`/users/${id}`);
+  export const fetchUser = id => async dispatch => {
+
+    const response = await jsonPlaceholder.get(`/users/${id}`);
 
       dispatch({ type: 'FETCH_USER', payload: response.data });
-  }
+  };
+
+// memoized version
+
+//   export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+//   const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//       dispatch({ type: 'FETCH_USER', payload: response.data });
+//   });
