1ea4a0c9633893bcd66380e5010c99f3dfbcc74f
brianrussick <brianrussick@gmail.com>
2021-05-14 12:50:55 -0400

    React_Rolodex_Search - RoboHash Web Service & JSON Placeholder

--- monsters_rolodex/src/components/card/card.js
+++ monsters_rolodex/src/components/card/card.js
@@ -4,7 +4,7 @@ import './card.styles.css';
 
 export const Card = props => (
     <div className='card-container'>
-        <img alt="monster" src={`https://robohash.org/{props.monster.id}?set=set2`} />
+        <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2`} />
         <h2> { props.monster.name } </h2>
         <p> {props.monster.email} </p>
     </div>
