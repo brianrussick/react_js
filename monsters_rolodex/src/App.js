5ee5681c0b06874c12d316bbbc162fc2c0af8e02
brianrussick <brianrussick@gmail.com>
2021-05-14 12:02:42 -0400

    React_Rolodex_Search - RoboHash Web Service & JSON Placeholder

---
+++ monsters_rolodex/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list';
+import { SearchBox } from './components/search-box/search-box';
+
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users}));
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
+
+      return (
+        <div className="App">
+        
+        <SearchBox 
+          placeholder='search monsters'
+          handleChange={e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters}>
+        
+        </CardList>  
+      </div>
+    );
+  }
+}
+
+export default App;
