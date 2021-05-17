fd26a18d6cbc219f5a9d58a732208f163da583c2
brianrussick <brianrussick@gmail.com>
2021-05-17 14:10:17 -0400

    React_Rolodex_Search - RoboHash Web Service & JSON Placeholder (final)

--- monsters_rolodex/src/App.js
+++ monsters_rolodex/src/App.js
@@ -20,6 +20,10 @@ class App extends Component {
     .then(users => this.setState({ monsters: users}));
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  }
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster => 
@@ -28,10 +32,10 @@ class App extends Component {
 
       return (
         <div className="App">
-        
+        <h1>Monsters Rolodex</h1>
         <SearchBox 
           placeholder='search monsters'
-          handleChange={e => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters}>
         
