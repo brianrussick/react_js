1be06999aae45c7397abe60a04fac07dbdd11cec
brianrussick <brianrussick@gmail.com>
2021-05-24 14:38:05 -0400

    React_E-Commerce_Web_App - up to date

--- e-commerce/src/components/menu-item/menu-item.js
+++ e-commerce/src/components/menu-item/menu-item.js
@@ -2,13 +2,20 @@ import React from 'react';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title }) => (
-    <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>{title}</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-);
-
-export default MenuItem;
+const MenuItem = ({ title, imageUrl, size }) => (
+    <div className={`${size} menu-item`}>
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
+      <div className='content'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
+    </div>
+  );
+  
+  export default MenuItem;
+  
