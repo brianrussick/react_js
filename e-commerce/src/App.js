68e2d297c60be5ce76f3f2bc592aa751e399e416
brianrussick <brianrussick@gmail.com>
2021-05-28 11:39:10 -0400

    React_E-Commerce_SPA - added SCSS styles etc.

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import './App.scss';
 
 import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
 
 const HatsPage = () => (
   <div>
@@ -16,7 +17,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
