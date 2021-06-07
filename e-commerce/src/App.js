c9e94cec95fa5ab0797fb9d0f094c1d5898b0bde
brianrussick <brianrussick@gmail.com>
2021-06-07 11:48:25 -0400

    React_E-Commerce_SPA - fixed Google sign-in

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -21,9 +21,24 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          },
+          () => { console.log(this.state);
+          });
+        });
+      }
+      this.setState({ currentUser: userAuth });
     });
+    
   }
 
   componentWillUnmount() {
