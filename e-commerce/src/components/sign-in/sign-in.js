e8a10c1e549f3631ebf3f403c8673b82aabebea3
brianrussick <brianrussick@gmail.com>
2021-06-08 11:34:40 -0400

    React_E-Commerce_SPA - Sign In & Sign Up page complete

--- e-commerce/src/components/sign-in/sign-in.js
+++ e-commerce/src/components/sign-in/sign-in.js
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input';
 import CustomButton from '../custom-button/custom-button';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -17,9 +17,17 @@ class SignIn extends React.Component {
       };
     }
   
-    handleSubmit = event => {
+    handleSubmit = async event => {
       event.preventDefault();
-  
+      
+      const { email, password } = this.state;
+
+      try {
+          await auth.signInWithEmailAndPassword(email, password);
+      } catch (error) {
+          console.log(error);
+      }
+
       this.setState({ email: '', password: '' });
     };
   
@@ -66,3 +74,4 @@ class SignIn extends React.Component {
   }
   
   export default SignIn;
+  
