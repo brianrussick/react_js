413e4070d22f82ebaceb4f2900e2bdf9ccdf0749
brianrussick <brianrussick@gmail.com>
2021-06-16 12:03:44 -0400

    React_E-Commerce_SPA - configured redux with shopping cart

--- e-commerce/src/redux/cart/cart.reducer.js
+++ e-commerce/src/redux/cart/cart.reducer.js
@@ -1,19 +1,27 @@
 import CartActionTypes from './cart.types';
+import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
-    hidden: true
-};
-
-const cartReducer = (state = INITIAL_STATE, action) => {
-    switch(action.type) {
-        case CartActionTypes.TOGGLE_CART_HIDDEN:
-            return {
-                ...state,
-                hidden: !state.hidden
-            }
-        default:
-            return state; 
+    hidden: true,
+    cartItems: []
+  };
+  
+  const cartReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+      case CartActionTypes.TOGGLE_CART_HIDDEN:
+        return {
+          ...state,
+          hidden: !state.hidden
+        };
+      case CartActionTypes.ADD_ITEM:
+        return {
+          ...state,
+          cartItems: addItemToCart(state.cartItems, action.payload)
+        };
+      default:
+        return state;
     }
-}
-
-export default cartReducer;
+  };
+  
+  export default cartReducer;
+  
