1656740d8a50d8f3a66a9664b9a9423b948c9169
brianrussick <brianrussick@gmail.com>
2021-06-18 11:58:44 -0400

    React_E-Commerce_SPA - added checkout page

--- e-commerce/src/components/cart-dropdown/cart-dropdown.js
+++ e-commerce/src/components/cart-dropdown/cart-dropdown.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { withRouter } from 'react-router-dom';
 
 import CustomButton from '../custom-button/custom-button';
 import CartItem from '../cart-item/cart-item';
@@ -7,19 +9,25 @@ import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems }) => (
+const CartDropdown = ({ cartItems, history }) => (
     <div className='cart-dropdown'>
-      <div className='cart-items'>
-        {cartItems.map(cartItem => (
+    <div className='cart-items'>
+      {cartItems.length ? (
+        cartItems.map(cartItem => (
           <CartItem key={cartItem.id} item={cartItem} />
-        ))}
+        ))
+      ) : (
+        <span className='empty-message'>Your cart is empty</span>
+      )}
       </div>
-      <CustomButton>GO TO CHECKOUT</CustomButton>
+      <CustomButton onClick={() => history.push('/checkout')}>
+          GO TO CHECKOUT
+      </CustomButton>
     </div>
   );
   
-  const mapStateToProps = state => ({
-    cartItems: selectCartItems(state)
+  const mapStateToProps = createStructuredSelector({
+    cartItems: selectCartItems
   });
   
-  export default connect(mapStateToProps)(CartDropdown);
+  export default withRouter(connect(mapStateToProps)(CartDropdown));
--- e-commerce/src/components/cart-icon/cart-icon.js
+++ e-commerce/src/components/cart-icon/cart-icon.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
@@ -19,12 +20,11 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => (
     toggleCartHidden: () => dispatch(toggleCartHidden())
   });
   
-  const mapStateToProps = state => ({
-    itemCount: selectCartItemsCount(state)
+  const mapStateToProps = createStructuredSelector({
+    itemCount: selectCartItemsCount
   });
   
   export default connect(
     mapStateToProps,
     mapDispatchToProps
   )(CartIcon);
-  
