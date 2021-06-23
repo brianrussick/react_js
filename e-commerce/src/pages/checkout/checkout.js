022002b31377922b4a5d499a39e6d837b0d25b3a
brianrussick <brianrussick@gmail.com>
2021-06-23 11:59:31 -0400

    React_E-Commerce_SPA - added checkout functionality

--- e-commerce/src/pages/checkout/checkout.js
+++ e-commerce/src/pages/checkout/checkout.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+
+import CheckoutItem from '../../components/checkout-item/checkout-item';
+
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
@@ -23,17 +26,12 @@ const CheckoutPage = ({ cartItems, total }) => (
             <div className='header-block'>
                 <span>Remove</span>
             </div>     
-        </div>
-        {
-            cartItems.map(cartItem =>
-                cartItem.name
-            )
-        }
-
-        <div className='total'>
-            <span>TOTAL: ${total}</span>
-        </div>
-    </div>
+            </div>
+    {cartItems.map(cartItem => (
+      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+    ))}
+    <div className='total'>TOTAL: ${total}</div>
+  </div>
 );
 
 const mapStateToProps = createStructuredSelector({
--- e-commerce/src/redux/cart/cart.reducer.js
+++ e-commerce/src/redux/cart/cart.reducer.js
@@ -1,5 +1,6 @@
 import CartActionTypes from './cart.types';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
+
 
 const INITIAL_STATE = {
     hidden: true,
@@ -18,6 +19,19 @@ const INITIAL_STATE = {
           ...state,
           cartItems: addItemToCart(state.cartItems, action.payload)
         };
+      case CartActionTypes.REMOVE_ITEM:
+        return {
+          ...state,
+          cartItems: removeItemFromCart(state.cartItems, action.payload)
+        }  
+
+      case CartActionTypes.CLEAR_ITEM_FROM_CART:
+        return {
+          ...state,
+          cartItems: state.cartItems.filter(
+            cartItem => cartItem.id !== action.payload.id
+            )
+        }
       default:
         return state;
     }
