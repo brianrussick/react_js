10333c24e34be72ac20b0ad459647f587d52ef34
brianrussick <brianrussick@gmail.com>
2021-06-30 11:49:18 -0400

    React_E-Commerce_SPA - updated collections

--- e-commerce/src/pages/shop/shop.js
+++ e-commerce/src/pages/shop/shop.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import { Route } from 'react-router-dom';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview';
+import CollectionsOverview from '../../components/collections-overview/collections-overview';
+import CollectionPage from '../collection/collection';
 
-import { selectCollections } from '../../redux/shop/shop.selectors';
+const ShopPage = ({ match }) => (
+  <div className='shop-page'>
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
-const ShopPage = ({ collections }) => (
-    <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-        </div>
-        );
-
-const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
