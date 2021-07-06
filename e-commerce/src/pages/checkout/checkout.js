13be4dd1cca4d245e6bd559cc1e9d01c329b5d3e
brianrussick <brianrussick@gmail.com>
2021-07-06 13:59:26 -0400

    React_E-Commerce_SPA - 1st version completed

--- e-commerce/src/pages/checkout/checkout.js
+++ e-commerce/src/pages/checkout/checkout.js
@@ -32,6 +32,11 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className='total'>TOTAL: ${total}</div>
+    <div className='test-warning'>
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: 01/25 - CCV: 123
+    </div>
     <StripeCheckoutButton price={total} />
   </div>
 );
