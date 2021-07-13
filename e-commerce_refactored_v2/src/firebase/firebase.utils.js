9e14547f86803d94ea126fd3cc09b7c363c98fea
brianrussick <brianrussick@gmail.com>
2021-07-13 11:28:02 -0400

    React_E-Commerce_SPA - updated firebase utils file

--- e-commerce_refactored_v2/src/firebase/firebase.utils.js
+++ e-commerce_refactored_v2/src/firebase/firebase.utils.js
@@ -52,6 +52,20 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
   return await batch.commit();
 }
 
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    }
+  });
+    console.log(transformedCollection);
+}
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
