0e9209b06e8e3c67f1b2ab215b5cc386396c2be3
brianrussick <brianrussick@gmail.com>
2021-07-23 11:01:31 -0400

    React_E-Commerce_SPA - added context to collection item

--- e-commerce_refactored_v2/src/components/cart-dropdown/cart-dropdown.js
+++ e-commerce_refactored_v2/src/components/cart-dropdown/cart-dropdown.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import React, { useContext } from 'react';
 import { withRouter } from 'react-router-dom';
 
 import CustomButton from '../custom-button/custom-button';
 import CartItem from '../cart-item/cart-item';
-import { selectCartItems } from '../../redux/cart/cart.selectors';
 import { toggleCartHidden } from '../../redux/cart/cart.actions'
+import { CartContext } from '../../providers/cart/cart.provider';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history, dispatch }) => (
+const CartDropdown = ({ history, dispatch }) => {
+  const { cartItems } = useContext(CartContext);
+  return (
     <div className='cart-dropdown'>
     <div className='cart-items'>
       {cartItems.length ? (
@@ -29,10 +29,6 @@ const CartDropdown = ({ cartItems, history, dispatch }) => (
           GO TO CHECKOUT
       </CustomButton>
     </div>
-  );
+  )};
   
-  const mapStateToProps = createStructuredSelector({
-    cartItems: selectCartItems
-  });
-  
-  export default withRouter(connect(mapStateToProps)(CartDropdown));
+  export default withRouter(CartDropdown);
