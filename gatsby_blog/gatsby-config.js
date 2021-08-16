a07c6311282ea8268f3fd41d70c84a6cf45726d6
brianrussick <brianrussick@gmail.com>
2021-08-16 11:03:57 -0400

    Gatsby Blog - implemented GraphQL

--- gatsby_blog/gatsby-config.js
+++ gatsby_blog/gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Brian's Blog`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
@@ -12,8 +12,16 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/images`
+      }
       },
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `markdowns`,
+          path: `${__dirname}/src/markdown-pages`
+          
+        },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
