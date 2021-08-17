fa0b668b12dde6183257f83ca6f8a436bc657385
brianrussick <brianrussick@gmail.com>
2021-08-17 11:05:29 -0400

    Gatsby Blog - configured GraphQL & added gatsby-tranformer-remark plug-in

--- gatsby_blog/gatsby-config.js
+++ gatsby_blog/gatsby-config.js
@@ -23,6 +23,7 @@ module.exports = {
           
         },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
--- gatsby_blog/src/pages/index.js
+++ gatsby_blog/src/pages/index.js
@@ -1,29 +1,36 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
+export default ({ data }) => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site!</p>
-    <p>Now go build something great!</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    <div>
+      <h1>Brian's Thoughts</h1>
+    </div>
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+  allMarkdownRemark {
+    totalCount
+    edges {
+      node {
+        id
+        frontmatter {
+          description
+          title
+          date
+        }   
+        excerpt
+      }
+    }
+  }
+}
+`
