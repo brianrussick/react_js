473ea3aca039e3eb9963b1dbc00949dd942c0511
brianrussick <brianrussick@gmail.com>
2021-08-20 10:57:14 -0400

    Gatsby Blog - updated blog page functionality

--- gatsby_blog/gatsby-node.js
+++ gatsby_blog/gatsby-node.js
@@ -1,3 +1,4 @@
+const path = require(`path`);
 const { createFilePath } = require (`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
@@ -11,4 +12,31 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
             value: slug
         })
     }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+    const { createPage } = actions
+    return graphql(`
+    {
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `).then(result => {
+        result.data.allMarkdownRemark.edges.forEach(({node}) => {
+            createPage({
+                path: node.fields.slug,
+                component: path.resolve(`./src/templates/blog-post.js`),
+                context: {
+                    slug: node.fields.slug
+                }
+            })
+        })
+    })
 }
