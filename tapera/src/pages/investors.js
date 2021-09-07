1853847eb4cb340a7bcd18ae68fdd4cccc8bf77d
brianrussick <brianrussick@gmail.com>
2021-09-07 13:43:04 -0400

    Tapera Website - Production build v5

--- tapera/src/pages/investors.js
+++ tapera/src/pages/investors.js
@@ -1,36 +1,53 @@
 export default function Investors() {
 
     return (
-      <div>
-            <div class="lg:flex lg:justify-center bg-white">
-            <div class="lg:flex-1 lg:mt-32">
-            <div><p class="text-5xl text-red-500 underline font-heading ml-2 lg:ml-10 lg:mb-0 ...">More Info…</p></div> 
-        </div>
-            
-        <div class="lg:space-y-4 font-opensans lg:absolute lg:bottom-0 lg:left-10 lg:mb-10 ...">
-            <div><p class="text-3xl ml-2 mt-6 lg:text-5xl lg:mt-0 lg:mb-10 lg:mr-0 text-blue-600 lg:ml-0 ...">What problems does your business solve?</p></div> 
-            <div><p class="text-3xl ml-2 mt-5 lg:text-2xl lg:ml-0 lg:mt-0 ...">Tapera is a new way for people to receive small amounts of money from</p></div>  
-            <div><p class="text-3xl ml-2  lg:text-2xl lg:ml-0 ...">those that have smartphones and spend it at normal point of sale systems</p></div> 
-            <div><p class="text-3xl ml-2  lg:text-2xl lg:ml-0 ...">or withdraw it from any ATM. Tapera does all of this using blockchain</p></div>
-            <div><p class="text-3xl ml-2  lg:text-2xl lg:ml-0 ...">technology with transaction times that are less than one second and cost</p></div>
-            <div><p class="text-3xl ml-2  lg:text-2xl lg:ml-0 ...">us less than one percent of the amount sent to process.</p></div>
-          </div> 
+      <section class="lg:container lg:px-6 lg:mx-auto lg:pt-32">
+      
 
-          <div class="mt-10 lg:flex-1 lg:h-16 lg:mt-32"> 
-          </div>
-           
 
-          <div class="divide-y-0 mt-14 my-3 lg:space-y-4 font-opensans lg:mr-8 lg:mt-32 ...">
-            <div><p class="text-3xl ml-2 lg:text-5xl lg:mb-10 lg:ml-0 text-blue-600 ...">Who is your target customer and or user?</p></div> 
-            <div><p class="text-2xl ml-2 mt-5 lg:text-2xl lg:ml-0 lg:mt-0 ...">Tapera's target customers are those that don't have access to</p></div>  
-            <div><p class="text-3xl ml-2  lg:text-2xl lg:ml-0 ...">smartphones (15% of US and 55% worldwide), people that don't have</p></div> 
-            <div><p class="text-3xl ml-2  lg:text-2xl lg:ml-0 ...">access to traditional banking services (3-4% of US household and 56% of</p></div>
-            <div><p class="text-2xl ml-2  lg:text-2xl lg:ml-0 ...">adults worldwide), and those that are early adopters and wish to use a</p></div>
-            <div><p class="text-2xl ml-2 mb-0 pb-8 lg:text-2xl lg:ml-0 lg:mb-96 lg:pb-32 ...">new anonymous form of cash.</p></div>
-          </div>
+
+
+    <div class="lg:grid">
+    <div class="p-4 bg-white rounded-lg">
+        
+         <div id="body" class="flex flex-col ml-5">
+            <h4 id="name" class="text-5xl font-semibold  mb-10 lg:mb-2 font-heading text-red-500">More Info…</h4>
+            <div class=" bg-gray-100 lg:flex lg:items-center">
+  
+    
+      
+      
+    
+    </div>
+            <h4 id="gard" class="text-5xl text-blue-500 font-semibold mb-2 mt-9">What problems does your business solve?</h4>
+        
+            <p id="text" class="text-2xl text-gray-800 mt-2">Tapera is a new way for people to receive small amounts of money from  
+            those that have smartphones and spend it at normal point of sale systems
+            or withdraw it from any ATM. Tapera does all of this using blockchain
+            technology with transaction times that are less than one second and cost
+            us less than one percent of the amount sent to process.</p>
             
-        </div>
-        </div>
+            <h4 id="gard" class="text-5xl text-blue-500 font-semibold mb-2 mt-20">Who is your target customer and or user?</h4>
+
+            <p id="text" class="text-2xl text-gray-800 mt-2">
+            Tapera's target customers are those that don't have access to  
+            smartphones (15% of US and 55% worldwide), people that don't have
+            access to traditional banking services (3-4% of US household and 56% of
+            adults worldwide), and those that are early adopters and wish to use a
+            new anonymous form of cash.</p>
+
+            <div class="flex mt-28">
+            </div>
+         </div>
+    </div>
+    </div>
+</section>
+
+
+
+
+
+
     );
   }
   
