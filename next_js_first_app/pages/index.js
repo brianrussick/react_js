faa8f5accebb57d737ae924a5b5db61f93f54b04
brianrussick <brianrussick@gmail.com>
2021-10-05 10:31:45 -0400

    Next JS - updated readme MD

--- next_js_first_app/pages/index.js
+++ next_js_first_app/pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Hello to the Next World!</h1>
+      <h1>HELLOOOOOOO to the Next World!</h1>
     </div>
   )
 }
