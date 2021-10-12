0c74b673cb3bc2b3a098ab76e491bdecc2f753c4
brianrussick <brianrussick@gmail.com>
2021-10-12 10:12:23 -0400

    Next JS - added routes

--- next_js_first_app/pages/clients/[id]/index.js
+++ next_js_first_app/pages/clients/[id]/index.js
@@ -4,10 +4,18 @@ import styles from '../../../styles/Home.module.css'
 export default function ClientProjects() {
     const router = useRouter();
     console.log(router.query);
-    
+
+    function loadProjectHandler() {}
+    // load data...
+    router.push({
+        pathname: '/clients/[id]/[id]',
+        query: { id: 'max', id: 'projecta' },
+    });
+
   return (
     <div className={styles.container}>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   )
 }
--- next_js_first_app/pages/clients/index.js
+++ next_js_first_app/pages/clients/index.js
@@ -1,9 +1,23 @@
+import Link from 'next/link';
 import styles from '../../styles/Home.module.css'
 
 export default function Clients() {
+    const clients = [
+        { id: "max", name: "Maximilian" },
+        { id: "manny", name: "Manuel" },
+    ];
+
   return (
     <div className={styles.container}>
       <h1>The Clients Page</h1>
+      <ul>
+          {clients.map((client) => (<li key={client.id}>
+          <Link href={`/clients/${client.id}`}>{client.name}</Link>
+
+          </li>))}
+          
+
+      </ul>
     </div>
   )
 }
