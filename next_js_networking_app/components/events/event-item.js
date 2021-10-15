ca5e993eb626c63c9632e4a8ecd45bb627a632a1
brianrussick <brianrussick@gmail.com>
2021-10-15 10:34:40 -0400

    Next JS Networking App - added header

--- next_js_networking_app/components/events/event-item.js
+++ next_js_networking_app/components/events/event-item.js
@@ -1,5 +1,6 @@
-import Link from 'next/link';
-
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
 import Button from '../ui/button';
 import classes from './event-item.module.css';
 
@@ -20,14 +21,19 @@ export default function EventItem(props) {
             <div className={classes.summary}>
                 <h2>{title}</h2>
                 <div className={classes.date}>
+                <DateIcon />
             <time>{humanReadableDate}</time>
             </div>
             <div className={classes.address}>
+            <AddressIcon />
                 <address>{formattedAddress}</address>
         </div>
         </div>
         <div className={classes.actions}>
-            <Button link={exploreLink}>Explore event</Button>
+            <Button link={exploreLink}>
+                <span>Explore event</span>
+                <span className={classes.icon}><ArrowRightIcon /></span>
+                </Button>
             </div>
         </div>
     </li>;
