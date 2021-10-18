c4e25389daf237a6a34af9cc920888896fc48b68
brianrussick <brianrussick@gmail.com>
2021-10-18 10:24:56 -0400

    Next JS Networking App - added search bar

--- next_js_networking_app/pages/events/index.js
+++ next_js_networking_app/pages/events/index.js
@@ -1,8 +1,26 @@
+import { useRouter } from "next/router";
+
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
+
+import { Fragment } from "react";
+
 export default function AllEventsPage() {
+  const router = useRouter();
+  const events = getAllEvents();
+  
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
+
     return (
-      <div>
-        <h1>All Events</h1>
-      </div>
+      <Fragment>
+        <EventsSearch onSearch={findEventsHandler} />
+        <EventList items={events} />
+      </Fragment>
     )
   }
   
