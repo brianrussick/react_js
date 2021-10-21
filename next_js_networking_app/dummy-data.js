0dfb00f6645beb3bbc2207c2aa6536c6b6b56e25
brianrussick <brianrussick@gmail.com>
2021-10-21 10:35:29 -0400

    Next JS Networking SPA - updated data file

--- next_js_networking_app/dummy-data.js
+++ next_js_networking_app/dummy-data.js
@@ -4,7 +4,7 @@ const DUMMY_EVENTS = [
       title: 'Programming for everyone',
       description:
         'Everyone can learn to code! Yes, everyone! In this live event, we are going to go through all the key basics and get you started with programming as well.',
-      location: 'Somestreet 25, 12345 San Somewhereo',
+      location: '2345 Lucky Lane, New York, NY 10007',
       date: '2021-05-12',
       image: 'images/coding-event.jpg',
       isFeatured: false,
@@ -14,7 +14,7 @@ const DUMMY_EVENTS = [
       title: 'Networking for introverts',
       description:
         "We know: Networking is no fun if you are an introvert person. That's why we came up with this event - it'll be so much easier. Promised!",
-      location: 'New Wall Street 5, 98765 New Work',
+      location: '1111 Ocean Ave, Santa Monica, CA 90401',
       date: '2021-05-30',
       image: 'images/introvert-event.jpg',
       isFeatured: true,
@@ -24,7 +24,7 @@ const DUMMY_EVENTS = [
       title: 'Networking for extroverts',
       description:
         'You probably need no help with networking in general. But focusing your energy correctly - that is something where most people can improve.',
-      location: 'My Street 12, 10115 Broke City',
+      location: '789 River Rd, Boulder, CO 80301',
       date: '2022-04-10',
       image: 'images/extrovert-event.jpg',
       isFeatured: true,
