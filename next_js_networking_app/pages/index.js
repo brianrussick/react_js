fcbbb66bd22d360805dce92070dd89232f385cdd
brianrussick <brianrussick@gmail.com>
2021-10-25 10:27:27 -0400

    Next JS Networking SPA - added Head section for home title

--- next_js_networking_app/pages/index.js
+++ next_js_networking_app/pages/index.js
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import { getFeaturedEvents } from "../dummy-data";
 import EventList from "../components/events/event-list";
 
@@ -6,6 +8,9 @@ export default function Home() {
 
   return (
     <div>
+      <Head>
+        <title>Next JS Networking SPA Web App</title>
+      </Head>  
       <EventList items={featuredEvents}/>
     </div>
   )
