c7b5c3f1af22f133e82a55acfa226e22c883255d
brianrussick <brianrussick@gmail.com>
2021-10-26 10:13:42 -0400

    Next JS Networking SPA - added title to Head in _app.js file

--- next_js_networking_app/pages/_app.js
+++ next_js_networking_app/pages/_app.js
@@ -7,6 +7,7 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return <Layout>
           <Head>
+            <title>Next Events</title>
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           </Head>
           <Component {...pageProps} />
