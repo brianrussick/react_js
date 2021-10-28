8cd91be932ef4a2fcc073eb4bca8f37cd387f71c
brianrussick <brianrussick@gmail.com>
2021-10-28 10:39:22 -0400

    Next JS Api Routes - first commit

---
+++ next_js_api_routes/pages/api/feedback.js
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import path from 'path';
+
+function handler(req, res) {
+    if (req.method === 'POST') {
+        const email = req.body.email;
+        const feedbackText = req.body.text;
+
+        const newFeedback = {
+            id: new Date().toISOString(),
+            email: email,
+            text: feedbackText,
+        };
+
+        // store that in a database or in a file
+        const filePath = path.join(process.cwd(), 'data', 'feedback.json');
+        const fileData = fs.readFileSync(filePath);
+        const data = JSON.parse(fileData);
+        data.push(newFeedback);
+        fs.writeFileSync(filePath, JSON.stringify(data));
+        res.status(201).json({ message: 'Success!', feedback: newFeedback });
+    }
+    res.status(200).json({ message: 'This works!' });
+}
+
+export default handler;
