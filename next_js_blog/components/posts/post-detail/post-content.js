6e0324d02fbc152b8b98af362f2e61b62bd5e870
brianrussick <brianrussick@gmail.com>
2021-11-11 10:01:58 -0500

    Next JS Blog - up-to-date

--- next_js_blog/components/posts/post-detail/post-content.js
+++ next_js_blog/components/posts/post-detail/post-content.js
@@ -2,17 +2,33 @@ import ReactMarkdown from "react-markdown";
 
 import PostHeader from "./post-header";
 import classes from './post-content.module.css';
-
+import Image from 'next/image';
 
 function PostContent(props) {
     const { post } = props;
 
     const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+    const customRenderers = {
+        
+        paragraph(paragraph) {
+            const { node } = paragraph;
+
+            if (node.children[0].type === 'image') {
+                const image = node.children[0];
+
+                return <div className={classes.image}>
+                    <Image src={`/images/posts/${post.slug}/${image.url}`} alt={image.alt} width={600} height={300} />
+                </div>
+            }
+            return <p>{paragraph.children}</p>;
+        }
+    };
+
     return (
         <article className={classes.content}>
             <PostHeader title={post.title} image={imagePath} />
-            <ReactMarkdown>{post.content}</ReactMarkdown>
+            <ReactMarkdown renderers={customRenderers} >{post.content}</ReactMarkdown>
         </article>
     )
 }
